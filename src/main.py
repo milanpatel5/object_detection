60fd91edf0af17f5887a502f74dd2bf0a4206213
milanpatel5 <code.milanpatel@gmail.com>
2020-05-03 19:28:48 +0530

    Implemented SSD architecture partially: Non-maximum Suppression step is remaining

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ import time
 import warnings
 
 from dateutil.relativedelta import relativedelta
-from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.layers import Conv2D, BatchNormalization, ReLU, SeparableConv2D, AveragePooling2D, Add
+from tensorflow.python.keras import Input
+from tensorflow.python.keras.layers import Conv2D, BatchNormalization, ReLU, SeparableConv2D, Add, Reshape, Softmax
+from tensorflow.python.keras.models import Model
 
 
 class BottleneckBlock:
@@ -44,7 +45,7 @@ class InvertedResidualBlock:
         return x
 
 
-class MobileNetV2():
+class MobileNetV2:
     def __call__(self, input):
         x = Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2), padding='same')(input)
         x = BatchNormalization(axis=3)(x)
@@ -55,24 +56,82 @@ class MobileNetV2():
         x = InvertedResidualBlock(t=6, c=32, n=3, s=2)(x)
         x = InvertedResidualBlock(t=6, c=64, n=4, s=2)(x)
         x = InvertedResidualBlock(t=6, c=96, n=3, s=1)(x)
-        x = InvertedResidualBlock(t=6, c=160, n=3, s=2)(x)
-        x = InvertedResidualBlock(t=6, c=320, n=1, s=1)(x)
+        x1 = InvertedResidualBlock(t=6, c=160, n=3, s=2)(x)
+        x2 = InvertedResidualBlock(t=6, c=320, n=1, s=1)(x1)
+        return x1, x2
 
-        x = Conv2D(filters=1280, kernel_size=(1, 1), padding='same')(x)
-        x = BatchNormalization(axis=3)(x)
-        x = ReLU(max_value=6)(x)
-        x = AveragePooling2D(pool_size=(7, 7))(x)
+
+class MultiScaleFeatureMaps:
+    def __init__(self):
+        pass
+
+    def __call__(self, input):
+        conv1 = Conv2D(filters=1024, kernel_size=(3, 3), activation='relu', padding='same')(input)
+        conv1 = Conv2D(filters=1024, kernel_size=(1, 1), activation='relu', padding='same')(conv1)
+
+        conv2 = Conv2D(filters=256, kernel_size=(1, 1), activation='relu', padding='same')(conv1)
+        conv2 = Conv2D(filters=512, kernel_size=(3, 3), activation='relu', padding='same', strides=2)(conv2)
+
+        conv3 = Conv2D(filters=128, kernel_size=(1, 1), activation='relu', padding='same')(conv2)
+        conv3 = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same', strides=2)(conv3)
+
+        conv4 = Conv2D(filters=128, kernel_size=(1, 1), activation='relu', padding='same')(conv3)
+        conv4 = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(conv4)
+
+        conv5 = Conv2D(filters=128, kernel_size=(1, 1), activation='relu', padding='same')(conv4)
+        conv5 = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(conv5)
+
+        return conv1, conv2, conv3, conv4, conv5
+
+
+class Predictors:
+    def __init__(self, n_boxes, n_classes):
+        self.n_boxes = n_boxes
+        self.n_classes = n_classes
+
+    def __call__(self, base_1, base_2, conv_1, conv_2, conv_3, conv_4, conv_5):
+        base_1_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(base_1)
+        base_1_cla = Reshape(target_shape=(base_1_cla.shape[1], base_1_cla.shape[2], self.n_boxes, self.n_classes))(base_1_cla)
+        base_1_cla = Softmax(axis=3)(base_1_cla)
+        base_1_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(base_1)
+        base_1_loc = Reshape(target_shape=(base_1_loc.shape[1], base_1_loc.shape[2], self.n_boxes, 4))
+
+        base_2_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(base_2)
+        base_2_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(base_2)
+
+        conv_1_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(conv_1)
+        conv_1_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(conv_1)
+
+        conv_2_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(conv_2)
+        conv_2_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(conv_2)
+
+        conv_3_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(conv_3)
+        conv_3_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(conv_3)
+
+        conv_4_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(conv_4)
+        conv_4_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(conv_4)
+
+        conv_5_cla = Conv2D(filters=self.n_boxes * self.n_classes, kernel_size=(3, 3), padding='same')(conv_5)
+        conv_5_loc = Conv2D(filters=self.n_boxes * 4, kernel_size=(3, 3), padding='same')(conv_5)
+
+
+class SingleShotDetector:
+    def __init__(self):
+        pass
+
+    def __call__(self, base_1, base_2):
+        conv_layers = MultiScaleFeatureMaps()(base_2)
+        x = Predictors(n_boxes=4, n_classes=100)(base_1, base_2, *conv_layers)
         return x
 
 
-def prep_model():
-    input = Input(shape=(224, 224, 3))
-    x = MobileNetV2()(input)
-    return Model(input, x)
-
-
 def main():
-    model = prep_model()
+    input = Input(shape=(512, 512, 3))
+
+    base_1, base_2 = MobileNetV2()(input)
+    output = SingleShotDetector()(base_1, base_2)
+
+    model = Model(input, output)
     model.compile()
     model.summary()
 
